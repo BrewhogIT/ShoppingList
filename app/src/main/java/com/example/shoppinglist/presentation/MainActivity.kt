f839bd4e254f75aa0bcd876f93dcd72d89085356
BrewhogIT <ssslll69@gmail.com>
2023-06-05 19:22:44 +0500

    create structure of project

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import com.example.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel.shopList.observe(this){
+            Log.d("testList",it.toString())
+        }
+
+        viewModel.getShopList()
+
+
+
+    }
+}
